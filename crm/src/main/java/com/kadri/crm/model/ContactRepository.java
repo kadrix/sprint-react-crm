4eb25e70cb20573cae73a87e2f57b89b5929b3ca
Islam <kadrii@outlook.com>
2019-11-08 21:09:33 -0600

    Added mock DB data and DB connection.

---
+++ crm/src/main/java/com/kadri/crm/model/ContactRepository.java
@@ -0,0 +1,11 @@
+package com.kadri.crm.model;
+import org.springframework.data.repository.CrudRepository;
+
+
+/*
+/ This interface acts as the connection between the model and the DB.
+/ Input: Contact and Id 
+*/
+public interface ContactRepository extends CrudRepository<Contact, Long>{
+
+}
---
+++ crm/src/main/java/com/kadri/crm/model/DemoLoader.java
@@ -0,0 +1,4 @@
+package com.kadri.crm.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
