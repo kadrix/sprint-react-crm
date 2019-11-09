af6abe211b5542cee8d49f7e94baab63a1fe413e
Islam <kadrii@outlook.com>
2019-11-08 21:10:54 -0600

    Added contact.

--- crm/src/main/java/com/kadri/crm/model/DemoLoader.java
+++ crm/src/main/java/com/kadri/crm/model/DemoLoader.java
@@ -2,3 +2,25 @@ package com.kadri.crm.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/*
+/ Contains test data to act as DB.
+/ Input: connection between model and DB.
+*/
+
+@Component
+public class DemoLoader implements CommandLineRunner{
+    private final ContactRepository repository;
+
+    @Autowired
+    public DemoLoader(ContactRepository repository){
+        this.repository = repository;
+    }
+
+    @Override 
+    public void run (String ...strings) throws Exception {
+        this.repository.save(new Contact("Joe", "Wags", "Joe@Wags.com"));
+    }
+
+}
